0b9193c828f1e3eb374d92b258fd6525a94c9574
fernandolilo <fernando.sistenpro@outlook.com>
2025-04-11 22:53:52 -0300

    print

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//um ex usando o console para escrita.
+Console.Write("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
