01d5acfaf4c06e267547776e5289248e8102f678
fernandolilo <fernando.sistenpro@outlook.com>
2025-04-12 10:55:56 -0300

    Loop for

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
-﻿class Program{
-    static void Main(){
-       
+﻿class Program
+{
+    static void Main()
+    {
 
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
 
-//um ex usando o console para escrita.
-Console.Write("Digite seu nome: ");
-string name = Console.ReadLine();
-Console.WriteLine(name);
-Console.WriteLine($"Olá {name}");
-Console.Write("Digite o ano de nascimento: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2025 - year;
-Console.WriteLine($"idade: {age}");
+        //um ex usando o console para escrita.
+        Console.Write("Digite seu nome: ");
+        string name = Console.ReadLine();
+        Console.WriteLine(name);
+        Console.WriteLine($"Olá {name}");
+        Console.Write("Digite o ano de nascimento: ");
+        int year = int.Parse(Console.ReadLine());
+        int age = 2025 - year;
+        Console.WriteLine($"idade: {age}");
 
-if(age >=18) Console.WriteLine("Você é maior de idade: ");
-else if(age == 38) Console.WriteLine($"Você tem {age}");
-else Console.WriteLine("voce é menor de idade");
+        if (age >= 18) Console.WriteLine("Você é maior de idade: ");
+        else if (age == 38) Console.WriteLine($"Você tem {age}");
+        else Console.WriteLine("voce é menor de idade");
 
+
+        string[] nome = { "Fernando", "Tania", "Elias", "Heloisa", "Davi" };
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(nome[i]);
+        }
     }
+
 }
