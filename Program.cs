da5f4b5665d178b98e7896c3ed98b428f3969b35
fernandolilo <fernando.sistenpro@outlook.com>
2025-04-11 23:10:48 -0300

    print e expressoes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+
 Console.WriteLine("Hello, World!");
 
 //um ex usando o console para escrita.
@@ -10,3 +12,7 @@ Console.Write("Digite o ano de nascimento: ");
 int year = int.Parse(Console.ReadLine());
 int age = 2025 - year;
 Console.WriteLine($"idade: {age}");
+
+if(age >=18) Console.WriteLine("Você é maior de idadae: ");
+else if(age == 38) Console.WriteLine($"Você tem {age}");
+else Console.WriteLine("voce é menor de idade");
