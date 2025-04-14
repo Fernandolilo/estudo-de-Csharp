4e8073ee8e03097c83b59e09bd9aabb410bc4775
fernandolilo <fernando.sistenpro@outlook.com>
2025-04-13 22:13:50 -0300

    Iniciando o OO

--- Program.cs
+++ Program.cs
@@ -4,32 +4,58 @@ class Program
 {
     static void Main()
     {
-        int[] numbers = { 1 };
-        try
-        {
-            //como tratar este erro por que a popsição do nosso index é 0
-            Console.WriteLine(numbers[1]);
-
-        }
-        catch (System.NullReferenceException ex)
-        {
-            Console.WriteLine($"Error, {ex.Message}, {ex.StackTrace}");
-        }
-        catch (System.IndexOutOfRangeException ex)
-        {
-            Console.WriteLine($"Error, {ex.Message}, {ex.StackTrace}");
-        }
-        catch (System.Exception ex)
-        {
-            Console.WriteLine($"Error, {ex.Message}, {ex.StackTrace}");
-
-        }
+        BankAcount acount = new BankAcount("Fernando", 100);
+        acount.Deposito(-50);
+        Console.WriteLine("Acount balance: " + acount.Balance);
+        BankAcount acount1 = new BankAcount("Tania", 100);
+        acount1.Deposito(100);
+        Console.WriteLine("Acount1 balance: " + acount1.Balance);
 
     }
 }
 
-//teste de acesso a class e inferencia de dados
-class Test()
+class BankAcount
 {
-    public int x;
+    private string name;
+    private decimal balance;
+
+    public decimal Balance
+    {
+        get{return balance;}
+
+    }
+
+    public string Name
+    {
+        get {return name;}
+        
+    }
+    
+    
+
+    public BankAcount(string name, decimal balance)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Nome invalido.", nameof(name));
+        }
+        if (balance <= 0)
+        {
+            throw new Exception("Saldo não pode ser negativo.");
+        }
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void Deposito(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            return;
+        }
+        balance += amount;
+    }
+
+
+
 }
